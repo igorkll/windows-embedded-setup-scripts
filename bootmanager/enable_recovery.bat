5533bdd3633218d7988c6e8a24325fb8749c04f9
igorkll <frerther@gmail.com>
2026-07-30 20:27:56 +0300

    added enable/disable recovery scripts

---
+++ bootmanager/enable_recovery.bat
@@ -0,0 +1,13 @@
+@echo off
+echo enable_recovery.bat
+
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Administrator rights are required. request...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+bcdedit /set {current} recoveryenabled yes
+
+pause
